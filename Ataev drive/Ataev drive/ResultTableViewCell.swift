6b4e3315df1027cbda4172ee40395bb9982a0bbf
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 11:50:54 +0300

    drive init

---
+++ Ataev drive/Ataev drive/ResultTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ResultTableViewCell.swift
+//  Ataev drive
+//
+//  Created by Атаев Магарам Никматулаевич on 23/1/2023.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var namePlayer: UILabel!
+    @IBOutlet weak var gameResult: UILabel!
+    @IBOutlet weak var timeResult: UILabel!
+    
+   
+}
